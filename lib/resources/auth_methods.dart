bc3ad5dce672f947d281da9df6bc4162799b7cfc
adogbeji <baka_1993@yahoo.co.uk>
2023-05-26 16:44:01 +0100

    Start creating function to signup user

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:typed_data';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign up user
+  signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    required Uint8List file
+  }) {}
 }
