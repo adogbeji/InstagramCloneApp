d89b1d8f252695618fd0ca119a9fb95fb503b8d9
adogbeji <baka_1993@yahoo.co.uk>
2023-05-29 16:20:39 +0100

    Add function return value & error message

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -6,11 +6,20 @@ class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // Sign up user
-  signUpUser({
+  Future<String> signUpUser({
     required String email,
     required String password,
     required String username,
     required String bio,
     required Uint8List file
-  }) {}
+  }) async {
+    String res = 'Some error occurred';
+
+    try {
+
+    } catch(err) {
+      res = err.toString();
+    }
+    return res;
+  }
 }
