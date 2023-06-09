240ca8bd42a08ac8e6420c0fb8b2d32068a42903
adogbeji <baka_1993@yahoo.co.uk>
2023-06-09 16:39:18 +0100

    Start adding code to add users to database

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:typed_data';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // Sign up user
   Future<String> signUpUser({
@@ -19,6 +21,17 @@ class AuthMethods {
       if (email.isNotEmpty || password.isNotEmpty || username.isNotEmpty || bio.isNotEmpty || file != null) {
         // Register User
         UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+
+        print(cred.user!.uid);
+        // Add user to database
+        _firestore.collection('users').doc(cred.user!.uid).set({
+          'username': username,
+          'uid': cred.user!.uid,
+          'email': email,
+          'bio': bio,
+          'followers': [],
+          'following': []
+        });
       }
     } catch(err) {
       res = err.toString();
